4a3eb9514273ce966b069343e51bd7d86f3d5c31
emanuel <emanuel.almeida1998@outlook.com>
2022-04-17 18:00:58 -0300

    math lib

--- scripts/src/class05/PessoaTeste.java
+++ scripts/src/class05/PessoaTeste.java
@@ -1,6 +1,6 @@
 package class05;
 
-public class Main {
+public class PessoaTeste {
 	public static void main(String[] args) {
 		Pessoa pessoa = new Pessoa(123, "jorginho", "m", "solteiro", 1.50, 70.0, true);
 		
